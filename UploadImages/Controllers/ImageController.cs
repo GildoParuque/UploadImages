074f838b559f8da7f2132a65e5a3de2f55b17ee7
IDGML <IDGML@MOZGMLIT05>
2021-02-08 00:44:38 -0800

    Add project files.

---
+++ UploadImages/Controllers/ImageController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using UploadImages.Models;
+
+namespace UploadImages.Controllers
+{
+    public class ImageController : Controller
+    {
+        private readonly ImageDbCOntext _context;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public ImageController(ImageDbCOntext context, IWebHostEnvironment hostEnvironment)
+        {
+            _context = context;
+            this._hostEnvironment = hostEnvironment;
+        }
+
+        // GET: Image
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Images.ToListAsync());
+        }
+
+        // GET: Image/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var imageModel = await _context.Images
+                .FirstOrDefaultAsync(m => m.ImageID == id);
+            if (imageModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(imageModel);
+        }
+
+        // GET: Image/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Image/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ImageID,Title,ImageFile")] ImageModel imageModel)
+        {
+            if (ModelState.IsValid)
+            {
+                string wwwRootPath = _hostEnvironment.WebRootPath;
+                string fileName = Path.GetFileNameWithoutExtension(imageModel.ImageFile.FileName);
+                string extension = Path.GetExtension(imageModel.ImageFile.FileName);
+                imageModel.ImageName = fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                string path = Path.Combine(wwwRootPath + "/Image/", fileName);
+                using (var fileStream = new FileStream(path,FileMode.Create))
+                {
+                    await imageModel.ImageFile.CopyToAsync(fileStream);
+                }
+
+                    _context.Add(imageModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(imageModel);
+        }
+
+        // GET: Image/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var imageModel = await _context.Images.FindAsync(id);
+            if (imageModel == null)
+            {
+                return NotFound();
+            }
+            return View(imageModel);
+        }
+
+        // POST: Image/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ImageID,Title,ImageName")] ImageModel imageModel)
+        {
+            if (id != imageModel.ImageID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(imageModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ImageModelExists(imageModel.ImageID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(imageModel);
+        }
+
+        // GET: Image/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var imageModel = await _context.Images
+                .FirstOrDefaultAsync(m => m.ImageID == id);
+            if (imageModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(imageModel);
+        }
+
+        // POST: Image/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var imageModel = await _context.Images.FindAsync(id);
+
+            //delete image from wwroot/image
+            var imagePath = Path.Combine(_hostEnvironment.WebRootPath, "Image",imageModel.ImageName);
+            if (System.IO.File.Exists(imagePath))
+                System.IO.File.Delete(imagePath);
+
+            //delete the record
+            _context.Images.Remove(imageModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ImageModelExists(int id)
+        {
+            return _context.Images.Any(e => e.ImageID == id);
+        }
+    }
+}
---
+++ UploadImages/Models/ImageDbCOntext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UploadImages.Models
+{
+    public class ImageDbCOntext : DbContext
+    {
+        public ImageDbCOntext(DbContextOptions<ImageDbCOntext> options): base(options)
+        {
+
+        }
+
+        public DbSet<ImageModel> Images { get; set; }
+    }
+}
